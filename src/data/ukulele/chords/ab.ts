76787fd96b3f8314bbdb81a631ededbb489bb181
Dmitri Sivachenko <10948158+dimasi@users.noreply.github.com>
2024-01-14 16:06:23 +0100

    Add ukulele chords

--- src/data/ukulele/chords/ab.ts
+++ src/data/ukulele/chords/ab.ts
@@ -2,16 +2,16 @@ import { TChord } from '@/domain/types';
 
 const chordsAb: TChord[] = [
   {
-    dots: [3, 1, 2, 1],
-    fret: 3,
+    dots: [1, 3, 4, 3],
+    fret: null,
     group: 'Ab',
     name: 'Ab',
   },
   {
-    dots: [4, 3, 4, 2],
-    fret: null,
+    dots: [0, 1, 2, 4],
+    fret: 3,
     group: 'Ab',
-    name: 'Abm/G#m',
+    name: 'Ab5',
   },
   {
     dots: [1, 3, 1, 3],
@@ -26,22 +26,106 @@ const chordsAb: TChord[] = [
     name: 'Ab7',
   },
   {
+    dots: [1, 3, 2, 1],
+    fret: null,
+    group: 'Ab',
+    name: 'Ab7sus2',
+  },
+  {
+    dots: [1, 3, 2, 4],
+    fret: null,
+    group: 'Ab',
+    name: 'Ab7sus4',
+  },
+  {
     dots: [1, 0, 2, 1],
     fret: null,
     group: 'Ab',
     name: 'Ab9',
   },
   {
-    dots: [1, 3, 1, 2],
+    dots: [1, 1, 2, 3],
     fret: null,
     group: 'Ab',
-    name: 'Abm6/G#m6',
+    name: 'Ab11',
+  },
+  {
+    dots: [3, 1, 1, 2],
+    fret: 8,
+    group: 'Ab',
+    name: 'Ab13',
+  },
+  {
+    dots: [4, 3, 4, 2],
+    fret: null,
+    group: 'Ab',
+    name: 'Abm',
+  },
+  {
+    dots: [1, 2, 1, 3],
+    fret: 4,
+    group: 'Ab',
+    name: 'Abm6',
   },
   {
     dots: [1, 3, 2, 2],
     fret: null,
     group: 'Ab',
-    name: 'Abm7/G#m7',
+    name: 'Abm7',
+  },
+  {
+    dots: [1, 2, 2, 2],
+    fret: null,
+    group: 'Ab',
+    name: 'Abm7b5',
+  },
+  {
+    dots: [1, 3, 2, 1],
+    fret: null,
+    group: 'Ab',
+    name: 'Abm9',
+  },
+  {
+    dots: [1, 3, 1, 1],
+    fret: 4,
+    group: 'Ab',
+    name: 'Abm11',
+  },
+  {
+    dots: [1, 1, 1, 1],
+    fret: null,
+    group: 'Ab',
+    name: 'Abm13',
+  },
+  {
+    dots: [3, 3, 4, 3],
+    fret: null,
+    group: 'Ab',
+    name: 'Abadd9',
+  },
+  {
+    dots: [1, 0, 0, 3],
+    fret: null,
+    group: 'Ab',
+    name: 'Abaug',
+  },
+  {
+    dots: [1, 4, 2, 3],
+    fret: null,
+    group: 'Ab',
+    name: 'Abaug7',
+  },
+  {
+    dots: [1, 2, 4, 2],
+    fret: null,
+    group: 'Ab',
+    name: 'Abdim',
+  },
+  {
+    dots: [1, 2, 1, 2],
+    fret: null,
+    group: 'Ab',
+    name: 'Abdim7',
   },
   {
     dots: [1, 3, 3, 3],
@@ -50,23 +134,34 @@ const chordsAb: TChord[] = [
     name: 'Abmaj7',
   },
   {
-    dots: [1, 2, 1, 2],
+    dots: [0, 0, 4, 1],
     fret: null,
     group: 'Ab',
-    name: 'Abdim/G#dim',
+    name: 'Abmaj9',
   },
   {
-    dots: [1, 0, 0, 3],
+    dots: [1, 3, 4, 1],
     fret: null,
     group: 'Ab',
-    name: 'Ab+/Abaug',
+    name: 'Absus2',
   },
   {
-    barre: [2, 2, 2, 2],
-    dots: [1, 1, 1, 1],
+    dots: [1, 3, 4, 4],
     fret: null,
     group: 'Ab',
-    name: 'Absus', // @todo добавить
+    name: 'Absus4',
+  },
+  {
+    dots: [4, 3, 4, 1],
+    fret: null,
+    group: 'Ab',
+    name: 'Abmadd9',
+  },
+  {
+    dots: [0, 3, 4, 2],
+    fret: null,
+    group: 'Ab',
+    name: 'AbmMaj7',
   },
 ];
 
--- src/data/ukulele/chords/b.ts
+++ src/data/ukulele/chords/b.ts
@@ -8,10 +8,10 @@ const chordsB: TChord[] = [
     name: 'B',
   },
   {
-    dots: [4, 2, 2, 2],
+    dots: [4, -1, 2, 2],
     fret: null,
     group: 'B',
-    name: 'Bm',
+    name: 'B5',
   },
   {
     dots: [1, 3, 2, 2],
@@ -20,16 +20,46 @@ const chordsB: TChord[] = [
     name: 'B6',
   },
   {
-    dots: [4, 3, 2, 0],
+    dots: [2, 3, 2, 2],
     fret: null,
     group: 'B',
     name: 'B7',
+  },
+  {
+    dots: [2, 1, 2, 2],
+    fret: null,
+    group: 'B',
+    name: 'B7sus2',
+  },
+  {
+    dots: [2, 4, 2, 2],
+    fret: null,
+    group: 'B',
+    name: 'B7sus4',
   },
   {
     dots: [2, 1, 3, 2],
     fret: 3,
     group: 'B',
     name: 'B9',
+  },
+  {
+    dots: [4, 3, 0, 0],
+    fret: null,
+    group: 'B',
+    name: 'B11',
+  },
+  {
+    dots: [4, 3, 4, 0],
+    fret: null,
+    group: 'B',
+    name: 'B13',
+  },
+  {
+    dots: [4, 2, 2, 2],
+    fret: null,
+    group: 'B',
+    name: 'Bm',
   },
   {
     dots: [1, 2, 2, 2],
@@ -44,29 +74,94 @@ const chordsB: TChord[] = [
     name: 'Bm7',
   },
   {
+    dots: [2, 2, 1, 2],
+    fret: null,
+    group: 'B',
+    name: 'Bm7b5',
+  },
+  {
+    dots: [3, 1, 4, 3],
+    fret: 2,
+    group: 'B',
+    name: 'Bm9',
+  },
+  {
+    dots: [4, 2, 0, 0],
+    fret: null,
+    group: 'B',
+    name: 'Bm11',
+  },
+  {
+    dots: [4, 2, 4, 0],
+    fret: null,
+    group: 'B',
+    name: 'Bm13',
+  },
+  {
+    dots: [4, 3, 2, 4],
+    fret: null,
+    group: 'B',
+    name: 'Badd9',
+  },
+  {
+    dots: [4, 3, 3, 2],
+    fret: null,
+    group: 'B',
+    name: 'Baug',
+  },
+  {
+    dots: [2, 3, 3, 2],
+    fret: null,
+    group: 'B',
+    name: 'Baug7',
+  },
+  {
+    dots: [4, 2, 1, 2],
+    fret: null,
+    group: 'B',
+    name: 'Bdim',
+  },
+  {
+    dots: [1, 2, 1, 2],
+    fret: null,
+    group: 'B',
+    name: 'Bdim7',
+  },
+  {
     dots: [4, 3, 2, 1],
     fret: null,
     group: 'B',
     name: 'Bmaj7',
   },
   {
-    dots: [1, 2, 1, 2],
-    fret: null,
+    dots: [2, 1, 4, 2],
+    fret: 3,
     group: 'B',
-    name: 'Bdim',
+    name: 'Bmaj9',
   },
   {
-    dots: [4, 3, 3, 2],
+    dots: [4, 1, 2, 2],
     fret: null,
     group: 'B',
-    name: 'B+/Baug',
+    name: 'Bsus2',
   },
   {
-    barre: [2, 2, 2, 2],
-    dots: [1, 1, 1, 1], //-
+    dots: [4, 4, 2, 2],
     fret: null,
     group: 'B',
-    name: 'Bsus', // @todo добавить
+    name: 'Bsus4',
+  },
+  {
+    dots: [4, 2, 2, 4],
+    fret: null,
+    group: 'B',
+    name: 'Bmadd9',
+  },
+  {
+    dots: [3, 2, 2, 2],
+    fret: null,
+    group: 'B',
+    name: 'BmMaj7',
   },
 ];
 
